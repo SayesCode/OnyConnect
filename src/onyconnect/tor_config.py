ab53c4da816324458ebb7bf7aa8e445db50bad73
I'm Matheus <codelong@proton.me>
2024-11-06 20:55:19 -0300

    Update tor_config.py

--- src/onyconnect/tor_config.py
+++ src/onyconnect/tor_config.py
@@ -3,17 +3,22 @@ import subprocess
 
 def generate_onion_service(port, directory):
     try:
-        service_dir = "/var/lib/tor/hidden_service/"
+        # Caminho para o diretório do serviço Onion
+        service_dir = "/tmp/hidden_service/"  # Ajustado para diretório temporário
         os.makedirs(service_dir, exist_ok=True)
 
+        # Lendo o template torrc e substituindo as variáveis
         with open("src/onyconnect/config/torrc_template", "r") as file:
             torrc_content = file.read().format(port=port, directory=directory)
         
+        # Criando o arquivo torrc
         with open("src/onyconnect/config/torrc", "w") as file:
             file.write(torrc_content)
 
+        # Executando o Tor
         subprocess.run(["tor", "-f", "src/onyconnect/config/torrc"])
 
+        # Lendo o hostname do serviço .onion gerado
         with open(os.path.join(service_dir, "hostname"), "r") as file:
             hostname = file.read().strip()
         
