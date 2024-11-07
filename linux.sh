9fec3a3ee94d94592e933081561795bd0487fb35
I'm Matheus <codelong@proton.me>
2024-11-07 14:19:22 -0300

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
 
-echo "Atualizando sistema e instalando dependências..."
-sudo apt-get update -y
-sudo apt-get install -y python3 python3-pip python3-venv tor
+# Verifica a arquitetura do sistema
+arch=$(uname -m)
+
+# Função para instalar o apt com o gerenciador de pacotes do sistema
+install_apt() {
+    echo "apt-get não encontrado. Tentando instalar o apt usando o gerenciador de pacotes do sistema..."
+
+    if command -v yum &> /dev/null; then
+        sudo yum install -y apt
+    elif command -v dnf &> /dev/null; then
+        sudo dnf install -y apt
+    elif command -v zypper &> /dev/null; then
+        sudo zypper install -y apt
+    elif command -v pacman &> /dev/null; then
+        sudo pacman -S --noconfirm apt
+    else
+        echo "Não foi possível encontrar um gerenciador de pacotes compatível para instalar o apt. Instale-o manualmente."
+        exit 1
+    fi
+
+    # Atualiza o índice de pacotes após a instalação do apt
+    sudo apt-get update -y
+}
+
+# Verificação do sistema e instalação dos pacotes necessários
+if [[ "$arch" == *'arm'* || "$arch" == *'Android'* ]]; then
+   pkg install -y tor
+   pkg install -y python3
+   pkg update && pkg upgrade -y
+else
+   if ! command -v apt-get &> /dev/null; then
+       install_apt
+   fi
+   echo "Atualizando sistema e instalando dependências..."
+   sudo apt-get update -y
+   sudo apt-get install -y python3 python3-pip python3-venv tor
+fi
 
 # Criação do ambiente virtual
 echo "Criando ambiente virtual..."
@@ -16,5 +50,6 @@ source venv/bin/activate
 echo "Instalando dependências do Python..."
 pip install fastapi jinja2 uvicorn python-multipart
 
+# Iniciando o script principal
 echo "Iniciando..."
 python src/onyconnect/main.py &
